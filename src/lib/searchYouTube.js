45ba5dbefc26872faabb7318e52b6d2427397bf9
Michael Tobias <michaelremitobias@gmail.com>
2020-09-01 12:30:05 -0700

    Working on connecting API

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  var options = {
+    q: q,
+    maxResults: 5,
+    key: 'AIzaSyAHyz-7XPtvuq8nOBdnnWPxo33jWHzd5Cs'
+  };
+  $.ajax({
+    url: Parse.server,
+    type: 'GET',
+    data: JSON.stringify(options),
+    contentType: 'application/json',
+    success: callback,
+    error: errorCB || function(error) {
+      console.error('Failed to pos message', error);
+    }
+  });
 };
 
 export default searchYouTube;
